96bfddceb008d115278e843166e7bb0ff9a37290
Shreiya Chowdhary <shreiya.c@gmail.com>
2018-11-10 19:43:09 -0500

    done

--- sketch.js
+++ sketch.js
@@ -5,36 +5,48 @@
 
 /* ===
 ml5 Example
-Basic Pitch Detection
+Webcam Image Classification using MobileNet and p5.js
+This example uses a callback pattern to create the classifier
 === */
 
-let audioContext;
-let mic;
-let pitch;
+let classifier;
+let video;
 
 function setup() {
   noCanvas();
-  audioContext = getAudioContext();
-  mic = new p5.AudioIn();
-  mic.start(startPitch);
+  // Create a camera input
+  video = createCapture(VIDEO);
+  // Initialize the Image Classifier method with MobileNet and the video as the second argument
+  classifier = ml5.imageClassifier('MobileNet', video, modelReady);
 }
 
-function startPitch() {
-  pitch = ml5.pitchDetection('./model/', audioContext, mic.stream, modelLoaded);
+function modelReady() {
+  // Change the status of the model once its ready
+  select('#status').html('Show me what you got');
+  // Call the classifyVideo function to start classifying the video
+  classifyVideo();
 }
 
-function modelLoaded() {
-  select('#status').html('Model Loaded');
-  getPitch();
+// Get a prediction for the current video frame
+function classifyVideo() {
+  classifier.predict(gotResult);
 }
 
-function getPitch() {
-  pitch.getPitch(function(err, frequency) {
-    if (frequency) {
-      select('#result').html(frequency);
-    } else {
-      select('#result').html('No pitch detected');
-    }
-    getPitch();
-  })
+// When we get a result
+function gotResult(err, results) {
+  // The results are in an array ordered by probability.
+  if (results[0].className == "banana" || results[0].className == "orange") {
+    console.log('yay!');
+    select('#message').html('Thank goodness!');
+    document.body.style.backgroundColor = "green";
+    document.body.style.color = "white";
+  } else {
+    select('#message').html('Show me something healthy!');
+    document.body.style.backgroundColor = "grey";
+    document.body.style.color = "white";
+  }
+  console.log("Results: ", results);
+  // select('#result').html(results[0].className);
+  // select('#probability').html(nf(results[0].probability, 0, 2));
+  classifyVideo();
 }
